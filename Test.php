143fd188356aa575ba5f58d6cf560ac4abe387aa
JORDANGUILLOU <62332478+JORDANGUILLOU@users.noreply.github.com>
2020-03-18 11:52:52 +0100

    Create Test.php

---
+++ Test.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Document</title>
+</head>
+
+<body>
+    <h2><a href="?message=Bonsoir"> Bonsoir</a>
+        <a href="?message=AHBAHNANENFAIT">Ah bah nan en fait</a>
+        <a href="?message=APLUSDANSLEBUS">A plus</a>
+    </h2>    
+    
+        
+    <?php
+    echo "Message est:".$_GET["texte"];
+        ?>
+</body>
+</html>
